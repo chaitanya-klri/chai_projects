57290156f5d02f024de6818eec46a35ecff125a3
chaitanya-klri <chaitanya.nitw@gmail.com>
2024-10-16 11:51:08 +0530

    Update app_skill.py

--- app_skill.py
+++ app_skill.py
@@ -155,7 +155,7 @@ def extract_data_from_pdf(uploaded_file):
 
 # Streamlit App
 st.title("Skill Based Summary")
-st.markdown("This facility is to be able to give you yearwise collection of skills by uploading the TMBs of a school for ASSET Pen and Paper.")
+st.markdown("This facility is to be able to give you yearwise collection of skills by uploading the TMBs of a school for ASSET Pen and Paper/AD.")
 
 # Upload PDFs
 uploaded_files = st.file_uploader("Upload PDF files. Note that you can attach TMBs of as many academic years from 2022 to 2024.", type="pdf", accept_multiple_files=True)
